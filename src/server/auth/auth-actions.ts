bc8f06ec4d9207f1ebf0d3ed68f4c67a5feda837
Abhishek Manikandan <32245193+abhishekngen@users.noreply.github.com>
2025-03-15 21:04:07 +0000

    google oauth

--- src/server/auth/auth-actions.ts
+++ src/server/auth/auth-actions.ts
@@ -43,7 +43,7 @@ export async function signInWithGoogle() {
     const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-            redirectTo: 'http://localhost:3000/auth/callback',
+            redirectTo: `${process.env.NEXT_PUBLIC_BASE_URL}/auth/callback`,
         }
     });
 
